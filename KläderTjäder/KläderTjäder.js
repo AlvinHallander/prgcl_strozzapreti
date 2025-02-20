b25cb108acc5c65669c4f8e52dc7f651f5c0e428
FeliciaLund <felicialund04@gmail.com>
2025-02-20 14:50:33 +0100

    Update KläderTjäder.js

---
+++ KläderTjäder/KläderTjäder.js
@@ -0,0 +1,17 @@
+//Lista över klädesplagg som kan rekommenderas
+const plagglista = ["Scarf", "Jeans", "Skjorta","Cowboyboots","Kjol"];
+
+// Slumpa ett plagg 
+function slumpaPlagg() {
+    const index = Math.floor(Math.random()*(plagglista.length));
+    return plagglista[index];
+}
+
+// Skriver ut vilket plagg som rekommenderas
+function visaRekommenderatPlagg() {
+    const plagg = slumpaPlagg();
+    document.write(`Tjädern rekommenderar: ${plagg}`);
+}
+
+// Kör funktionen när sidan laddats
+window.onload = visaRekommenderatPlagg();
